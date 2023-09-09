f8335e61d0e4275e1544b90db7db62b2d8fad75a
arichorn <78001398+arichorn@users.noreply.github.com>
2023-09-09 14:26:42 -0500

    Update Buttons.java

--- src/main/java/dev/redy1aye/copperequipment/blocks/Buttons.java
+++ src/main/java/dev/redy1aye/copperequipment/blocks/Buttons.java
@@ -1,9 +1,18 @@
 package dev.redy1aye.copperequipment.blocks;
 
-import net.minecraft.block.StoneButtonBlock;
+import net.minecraft.block.AbstractButtonBlock;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 
-public class Buttons extends StoneButtonBlock {
-    public Buttons(Settings settings) {
+public class ButtonBlock extends AbstractButtonBlock {
+    public ButtonBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    protected SoundEvent getClickSound(boolean powered) {
+        return powered ? SoundEvents.BLOCK_STONE_BUTTON_CLICK_ON : SoundEvents.BLOCK_STONE_BUTTON_CLICK_OFF;
     }
 }
